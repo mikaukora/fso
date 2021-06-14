9c90a1dacdd74dea73af7f8527bc1ff71af41df5
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-06-14 22:53:44 +0300

    Up to exercise 1.2

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} exercises={props.exercises[0]}></Part>
+      <Part part={props.parts[1]} exercises={props.exercises[1]}></Part>
+      <Part part={props.parts[2]} exercises={props.exercises[2]}></Part>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.total}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      ></Content>
+      <Total total={exercises1 + exercises2 + exercises3}></Total>
+    </div>
+  );
+};
+
+export default App;
