46c527512d3e1fd4c4ce2744d914f2c6f3713b28
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-02 14:42:38 +0300

    4.4

--- part4/.eslintrc.js
+++ part4/.eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
   extends: 'eslint:recommended',
+  plugins: ['jest'],
   parserOptions: {
     ecmaVersion: 12,
   },
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 }
 
+const totalLikes = (blogs) => {
+  if (blogs) {
+    const total = blogs.reduce((acc, blog) => acc + blog.likes, 0);
+    return total;
+  }
+  return 0;
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
