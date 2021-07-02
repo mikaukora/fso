d3d6ebd80cc8c0b72881e6d310c960f3890ee00f
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-02 12:27:36 +0300

    4.2

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,24 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const logger = require('../utils/logger');
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+  logger.info(blog);
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = blogsRouter
