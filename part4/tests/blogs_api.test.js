9bb7ae3ce9139d092000775d2ca64e16aea3c0ff
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-05 10:37:11 +0300

    4.8, 4.9

---
+++ part4/tests/blogs_api.test.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+describe('get blogs', () => {
+  const initialBlogs = [
+    {
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+    },
+    {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+    },
+  ]
+
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    let noteObject = new Blog(initialBlogs[0]);
+    await noteObject.save();
+    noteObject = new Blog(initialBlogs[1]);
+    await noteObject.save();
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  })
+
+  test('there are correct amount of notes', async () => {
+    const response = await api.get('/api/blogs');
+    const titles = response.body.map(r => r.title);
+
+    expect(response.body).toHaveLength(initialBlogs.length);
+    expect(titles).toEqual(
+      ['Type wars', 'Canonical string reduction']
+    );
+  })
+
+  test('id field has correct name', async () => {
+    const response = await api.get('/api/blogs');
+
+    const firstBlog = response.body[0];
+    expect(firstBlog.id).toBeDefined();
+    expect(firstBlog._id).not.toBeDefined();
+  })
+
+  afterAll(() => {
+    mongoose.connection.close();
+  })
+
+});
