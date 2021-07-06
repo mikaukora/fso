bf90de4f2fe8a48739a4080ff7802fa01d703463
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-06 13:12:02 +0300

    4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,31 +1,46 @@
+const config = require('../utils/config');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username:1, name:1, id:1 });
   response.json(blogs.map(blog => blog.toJSON()));
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, config.SECRET);
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
 
   // Temporarily pick the first one
-  const someUser = await User.findOne({});
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
     url: body.url,
     likes: body.likes,
     author: body.author,
-    user: someUser._id
+    user: user._id
   }
   );
 
   const savedBlog = await blog.save();
-  console.log('someUser; ', someUser);
-  someUser.blogs = someUser.blogs.concat(savedBlog._id);
-  await someUser.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   response.status(201).json(savedBlog.toJSON());
 })
 
