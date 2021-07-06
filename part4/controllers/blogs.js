8b9fe50d8a99544ab336f36355b2c008aeb8cd85
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-06 14:01:45 +0300

    4.20

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -9,24 +9,14 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()));
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
-  const token = getTokenFrom(request);
-  const decodedToken = jwt.verify(token, config.SECRET);
+  const decodedToken = jwt.verify(request.token, config.SECRET);
 
-  if (!token || !decodedToken.id) {
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
-  // Temporarily pick the first one
   const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
