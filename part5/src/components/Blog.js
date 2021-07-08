2dc8e7b6fc8aed2739fc0a1f4b7e05c3c99ddee6
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-08 16:57:33 +0300

    5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,34 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react';
+
+const Blog = ({blog}) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetails = () => setShowDetails(!showDetails);
+
+  const titles = () => ( <div style={blogStyle}> {blog.title} {blog.author}  <button onClick={toggleDetails}>view</button></div> );
+  const details = () => (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author}
+      <p>{blog.url}</p>
+      <p> likes: {blog.likes} <button>like</button></p>
+      <p> {blog?.user?.name}</p>
+      <button onClick={toggleDetails}>hide</button>
+      </div>
+    );
+
+  return (
+    <div>
+    {showDetails ? details() : titles()}
+    </div>
+  )
+}
 
 export default Blog
