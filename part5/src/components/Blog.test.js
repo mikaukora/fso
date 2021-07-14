f9732d061edbab6a0738d8b40dab64cf63cd7317
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-14 11:50:34 +0300

    5.14

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { prettyDOM } from '@testing-library/dom';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
 test('renders default content', () => {
@@ -34,4 +35,48 @@ test('renders default content', () => {
   expect(component.container).not.toHaveTextContent(
     blog.url
   );
+
+  const blogElem = component.container.querySelector('.blog');
+  console.log(prettyDOM(blogElem));
+});
+
+
+test('renders details', async () => {
+  const blog = {
+    title: 'test title',
+    author: 'some ',
+    likes: 100,
+    url: 'https//www.google.com',
+    user: { username: '123' },
+  };
+
+  const mockOnRemove = jest.fn();
+  const mockOnLike = jest.fn();
+  const user = '123';
+
+  const component = render(
+    <Blog blog={blog} onRemove={mockOnRemove} onLike={mockOnLike} currentUser={user}/>
+  );
+
+  const button = component.getByText('view');
+  fireEvent.click(button);
+
+  expect(component.container).toHaveTextContent(
+    blog.title
+  );
+
+  expect(component.container).toHaveTextContent(
+    blog.author
+  );
+
+  expect(component.container).toHaveTextContent(
+    blog.likes
+  );
+
+  expect(component.container).toHaveTextContent(
+    blog.url
+  );
+
+  const blogElem = component.container.querySelector('.blog');
+  console.log(prettyDOM(blogElem));
 });
