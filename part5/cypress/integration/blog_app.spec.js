5dd6cd8022ed36d07b9ea8383074ee5166bbf499
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-15 14:35:05 +0300

    5.21

--- part5/cypress/integration/blog_app.spec.js
+++ part5/cypress/integration/blog_app.spec.js
@@ -46,37 +46,79 @@ describe('Blog ', function() {
     cy.contains('Test User logged in');
   });
 
-  describe.only('When logged in', function() {
-    beforeEach(function() {
-      cy.contains('Login').click();
-      cy.get('#username').type('tuser');
-      cy.get('#password').type('salainen');
-      cy.get('#login-button').click();
-    });
+});
 
-    it('A blog can be created', function() {
-      cy.contains('create new blog').click();
-      cy.get('#title').type('a note created by cypress');
-      cy.get('#author').type('Cypress');
-      cy.get('#url').type('https://foo.bar.com');
 
-      cy.get('#submit-button').click();
-      cy.contains('a note created by cypress');
-    });
+describe('When logged in', function() {
+  before(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
 
-    it('A blog can be liked', function() {
-      cy.contains('create new blog').click();
-      cy.get('#title').type('a note created by cypress');
-      cy.get('#author').type('Cypress');
-      cy.get('#url').type('https://foo.bar.com');
+    const user = {
+      name: 'Test User',
+      username: 'tuser',
+      password: 'salainen'
+    };
 
-      cy.get('#submit-button').click();
-      cy.contains('view').click();
-      cy.contains('likes: 0');
+    cy.request('POST', 'http://localhost:3003/api/users/', user);
 
-      cy.contains('like').click();
-      cy.contains('likes: 1');
+    cy.visit('http://localhost:3000');
+    cy.contains('Login').click();
+    cy.get('#username').type('tuser');
+    cy.get('#password').type('salainen');
+    cy.get('#login-button').click();
 
-    });
   });
+
+  it('A blog can be created', function() {
+    cy.contains('create new blog').click();
+    cy.get('#title').type('a note created by cypress');
+    cy.get('#author').type('Cypress');
+    cy.get('#url').type('https://foo.bar.com');
+
+    cy.get('#submit-button').click();
+    cy.contains('a note created by cypress');
+  });
+
+  it('A blog can be liked', function() {
+    cy.contains('create new blog').click();
+    cy.get('#title').type('another note created by cypress');
+    cy.get('#author').type('Cypress');
+    cy.get('#url').type('https://foo.bar.com');
+
+    cy.get('#submit-button').click();
+    cy.contains('view').click();
+    cy.contains('likes: 0');
+
+    cy.contains('like').click();
+    cy.contains('likes: 1');
+  });
+
+  it('A blog can be removed', function() {
+    cy.contains('view').click();
+    cy.contains('Remove').click();
+    cy.get('.info').contains('a note created by cypress by Cypress removed');
+    cy.get('html').should('not.contain', 'a note created by cypress');
+  });
+
+  it('A blog cant be removed as wrong user', function() {
+    cy.contains('Log out').click();
+
+    const user = {
+      name: 'Wrong User',
+      username: 'wuser',
+      password: 'supersalainen'
+    };
+
+    cy.request('POST', 'http://localhost:3003/api/users/', user);
+
+    cy.visit('http://localhost:3000');
+    cy.contains('Login').click();
+    cy.get('#username').type(user.username);
+    cy.get('#password').type(user.password);
+    cy.get('#login-button').click();
+
+    cy.contains('view').click();
+    cy.get('html').should('not.contain', 'Remove');
+  });
+
 });
