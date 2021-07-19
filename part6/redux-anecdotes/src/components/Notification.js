5dd6e0439c568eac36c27dd8737ba46a577be623
Mika Kaukoranta <mika.kaukoranta@gmail.com>
2021-07-19 22:27:41 +0300

    6.19

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,10 +1,9 @@
 
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 
-const Notification = () => {
-  // Take the first (newest) note only
-  const notification = useSelector(state => state.notification[0]);
+const Notification = (props) => {
+  const notification = props.notification;
 
   const style = {
     border: 'solid',
@@ -18,5 +17,13 @@ const Notification = () => {
     <div>{notification?.text ? noteBox() : null}</div>
   );
 }
+const mapStateToProps = (state) => {
+  return {
+    // Take the first (newest) note only
+    notification: state.notification[0],
+  }
+}
 
-export default Notification;
+const ConnectedNotification = connect(mapStateToProps)(Notification);
+
+export default ConnectedNotification;
